8e9327a96c5833ed51fb16bb10ecfcdcaa118449
Bridger Jacobs <115174637+Gingersnapp25@users.noreply.github.com>
2023-04-24 09:36:56 -0700

    Cabinet + Animation has been added!

---
+++ The Peanut Game/Assets/Facility Models/CabinetAnimation.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinetAnimation : MonoBehaviour
+{
+    Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown("up"))
+        //{
+        //m_Animator.SetTrigger("Open");
+        //}
+
+        //if (Input.GetKeyDown("down"))
+        //{
+        //    m_Animator.SetTrigger("Close");
+        //}
+    }
+
+    public void Open()
+    {
+        m_Animator.SetTrigger("Open");
+    }
+
+    public void close()
+    {
+        m_Animator.SetTrigger("Close");
+    }
+}
