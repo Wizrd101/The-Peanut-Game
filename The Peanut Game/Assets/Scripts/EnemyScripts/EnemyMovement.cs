3ab70765d725dad28741b8b46fd3ac83e7820026
Simon Henderson <simonhenderson100@gmail.com>
2023-04-26 08:50:38 -0700

    I guess I haven't saved in a while, so save push I guess
    Still working on the AI. I found a new tutorial that suits the situation better, so I updated it a bit. Also, for whatever reason, the AI tabs in the Windows were removed and placed in a package, which I admit is where most of my time went trying to figure that out lol. So, a new package has been downloaded.

--- The Peanut Game/Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ The Peanut Game/Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -3,58 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+// Followed a video tutorial: https://www.youtube.com/watch?v=c8Nq19gkNfs
+
 public class EnemyMovement : MonoBehaviour
 {
+    // Declaring the variables
     public NavMeshAgent agent;
 
-    public Transform player;
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target;
 
-    public LayerMask isGround, isPlayer;
+    public float triggerDist = 1;
 
-    //Patroling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
-
-    //States
-    public float sightRange;
-    public bool playerInSightRange;
-
-    private void Awake()
+    void Start()
     {
-        player = GameObject.Find("PlayerCapsule").transform;
         agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
     }
 
-    private void Update()
+    void Update()
     {
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, isPlayer);
-
-        if (!playerInSightRange)
+        // If we are close enough to the waypoint, change to the next waypoint
+        if (Vector3.Distance = (target.position, target) < triggerDist)
         {
-            Patroling();
-        }
-        else
-        {
-            Chasing();
+            IterateWaypointIndex();
+            UpdateDestination();
         }
     }
 
-    private void Patroling()
+    // Changes between the waypoints
+    void IterateWaypointIndex()
     {
-        if (!walkPointSet)
+        // Simply selects the next waypoint in the list
+        waypointIndex++;
+        if (waypointIndex == waypoints.Length)
         {
-            SearchWalkPoint();
+            waypointIndex = 0;
         }
+
+        // OPTION FOR IMPROVEMENT: Make the system select a random waypoint everytime IterateWaypointIndex is called
+            // SUB-OPTION: Make the system only select a random waypoint within a certain distance of the player, to keep the enemy close to the player
     }
 
-    private void Chasing()
+    // Updates where the NavMeshAgent is tracking
+    void UpdateDestination()
     {
-
-    }
-
-    private void SearchWalkPoint()
-    {
-        
+        target = waypoints[waypointIndex].position;
+        agent.SetDestination(target);
     }
 }
