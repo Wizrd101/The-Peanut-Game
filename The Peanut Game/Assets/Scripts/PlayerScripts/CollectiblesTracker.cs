41253b8073a24a50fe214c90f1d3dbd8f48b3f53
Simon Henderson <simonhenderson100@gmail.com>
2023-04-28 10:00:06 -0700

    Implementing a very bad collectible pickup system
    NOW WITH RAYCASTS!!!
    (In case it wasn't obvious, I'm glad we're actually visiting the coding sections. I've heard a little bit about the raycasts, and they are pretty powerful)
    Anyways, the system is going to use Raycasts instead of collisions to get the collectibles. I might be leaving soon, so this is also like an end of the day push, and I just wanted to say that if it gives you errors, feel free to go in and comment all of it out to stop them lol. It's very bad now, mostly because I haven't tested it at all.

--- The Peanut Game/Assets/Scripts/PlayerScripts/CollectiblesTracker.cs
+++ The Peanut Game/Assets/Scripts/PlayerScripts/CollectiblesTracker.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CollectiblesTracker : MonoBehaviour
 {
+    // Temporary Collectibles Variables, to make it easier to transfer to PlayerPrefs
     public int tempCollectibles;
+
+    // Distance that the player can interact with collectibles
+    public float collectibleRaycastDistance = 30;
+
+    // Canvas that shows when the player is looking at a collectible
+    // Basically tells the player "Hey, you can pick this up!"
+    public Canvas colCanvas;
 
     void Start ()
     {
         tempCollectibles = 0;
         PlayerPrefs.SetInt("Collectibles", 0);
+        colCanvas.enabled = false;
     }
 
-    void OnTriggerEnter (Collider other)
+    // Old system, outdated. Going to use Raycasts instead.
+    /*void OnTriggerEnter (Collider other)
     {
         if (other.tag == "Collectible")
         {
@@ -20,6 +30,29 @@ public class CollectiblesTracker : MonoBehaviour
             PlayerPrefs.SetInt("Collectibles", tempCollectibles);
             Debug.Log(PlayerPrefs.GetInt("Collectibles"));
             Destroy(other.gameObject);
+        }
+    }*/
+
+    // New system (with Raycasts)
+    void Update ()
+    {
+        RaycastHit hit;
+        Ray CDRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if (Physics.Raycast(ray, out hit, collectibleRaycastDistance))
+        {
+            if (hit.collider.gameObject.tag == "Collectible")
+            {
+                colCanvas.enabled = true;
+                if (Input.GetButtonDown("E"))
+                {
+                    tempCollectibles++;
+                    hit.collider.gameObject.Destroy();
+                }
+            }
+            else
+            {
+                colCanvas.enabled = false;
+            }
         }
     }
 }
