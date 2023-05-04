353bdd3c8bdad924dce426c061c09ae277b088d5
Simon Henderson <simonhenderson100@gmail.com>
2023-05-04 09:15:54 -0700

    Merge branch 'main' of https://github.com/Wizrd101/The-Peanut-Game

--- The Peanut Game/Assets/Scripts/OtherStuff/LevelOneNextLevelTrigger.cs
+++ The Peanut Game/Assets/Scripts/OtherStuff/LevelOneNextLevelTrigger.cs
@@ -5,11 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LevelOneNextLevelTrigger : MonoBehaviour
 {
+    BinarySave bs;
+
+    GameObject colOne;
+    GameObject colTwo;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            if (colOne == null)
+            {
+                bs.gameData.collectibleOneCollected = true;
+            }
 
+            if (colTwo == null)
+            {
+                bs.gameData.collectibleTwoCollected = true;
+            }
             //Debug.Log("Trigger Next Level");
             SceneManager.LoadScene("Levelthreescene");
         }
