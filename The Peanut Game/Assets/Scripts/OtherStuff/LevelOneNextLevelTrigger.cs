6eb1821bf5b69b87e92212530ed1cb27f72b535d
Simon Henderson <simonhenderson100@gmail.com>
2023-05-08 09:09:12 -0700

    Messed around in the instructions scene
    There is now a table with a single piece of paper on it. If you interact with that piece of paper (like you would a collectible), then you will start the game.

--- The Peanut Game/Assets/Scripts/OtherStuff/LevelOneNextLevelTrigger.cs
+++ The Peanut Game/Assets/Scripts/OtherStuff/LevelOneNextLevelTrigger.cs
@@ -5,34 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelOneNextLevelTrigger : MonoBehaviour
 {
-    public GameObject Player; 
-
-    BinarySave bs;
-
-    public GameObject colOne;
-    public GameObject colTwo;
-
-    void Start()
-    {
-        bs = Player.GetComponent<BinarySave>();
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            if (colOne == null)
-            {
-                bs.gameData.collectibleOneCollected = true;
-                Debug.Log("Collectible 1 Gotten");
-            }
-
-            if (colTwo == null)
-            {
-                bs.gameData.collectibleTwoCollected = true;
-                Debug.Log("Collectible 2 Gotten");
-            }
-            //Debug.Log("Trigger Next Level");
             SceneManager.LoadScene("Leveltwoscene");
         }
     }
